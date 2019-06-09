c415a06f58aa3c1f7718041c8e3d35c1e241bae2
Eren Dogan <eren.dogan@gaziulas.com.tr>
2019-06-09 15:43:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-```js
-const obj = { foo: "bar" };
+var recipes = {};
 
-const newObj = Object.assign({}, obj);
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = Object.assign({}, object);
+  newObj[key] = value;
+  return newObj;
+}
+updateObjectWithKeyAndValue({prop: 1}, 'prop2', 2);
 
-newObj;
-// => { foo: "bar" }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+destructivelyUpdateObjectWithKeyAndValue({prop: 1}, 'prop2', 2);
 
-delete newObj.foo;
-// => true
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj[key];
+}
+deleteFromObjectByKey({prop: 1}, 'prop');
 
-newObj;
-// => {}
-
-obj;
-// => { foo: "bar" }
-```
-
-var driver = [];
+destructivelyDeleteFromObjectByKey(object, key){
+  var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+  return obj[key];
+}
