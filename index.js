6913c073a8caf1536ec606fec642606620edcbc6
Eren Dogan <eren.dogan@gaziulas.com.tr>
2019-06-09 15:05:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = Object.assign({}, object);
+  newObj[key] = value;
+  return newObj;
+}
+updateObjectWithKeyAndValue({prop: 1}, 'prop2', 2);
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+destructivelyUpdateObjectWithKeyAndValue({prop: 1}, 'prop2', 2);
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj[key];
+}
+deleteFromObjectByKey({prop: 1}, 'prop');
+
+destructivelyDeleteFromObjectByKey(object, key){
+  var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+  return obj[key];
+}
