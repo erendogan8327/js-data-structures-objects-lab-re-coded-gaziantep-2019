c439d396c100c13cc689d8c5baf41ccfa5ea5382
Eren Dogan <eren.dogan@gaziulas.com.tr>
2019-06-09 15:55:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 let driver={name:'Sam'};
-function updateDriverWithKeyAndValue(driver, key, value){
+let newdriver={};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
   
  return Object.assign({},driver, { [key]: value });
   
 }
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  
   driver[key] = value;
   return driver;
+  
 }
-function deleteFromDriverByKey(driver, key){
-  let newdriver={};
+function deleteFromDriverByKey(driver, key) {
+  
+
   newdriver= delete driver.key;
   return newdriver;
+  
 }
-function destructivelyDeleteFromDriverByKey(driver, key){
-  let newdriver={};
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  
+
   newdriver= delete driver[key];
   delete driver[key];
   return driver;
+  
 }
 
 
